6f3eef1b546f5f857c0499a8bd093fec71db32f4
sachioramiro <101619321+sachioramiro@users.noreply.github.com>
2022-03-28 16:17:01 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo"nama sachioramiro
+        tinggal depok 
+        olahraga bola";
+    }
+}
