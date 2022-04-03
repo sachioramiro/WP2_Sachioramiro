1cd153fab2469375d10b236e70969651a0b9e8ad
sachioramiro <101619321+sachioramiro@users.noreply.github.com>
2022-04-03 16:25:20 +0700

    Add files via upload

---
+++ view-latihan1.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>latihan 1</title>
+</head>
+
+<body>
+    Haloo Kawan..yuk kita bealajar web progamming..!!!<br>
+    nilai 1 = <?= $nilai1; ?> <br>
+    milai 2 = <?= $nilai2; ?> <br>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " .
+         $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
